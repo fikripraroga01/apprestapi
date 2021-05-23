ffd5c1240174418c1883c5fa1283a0c813da9d8a
Fikri Prayoga <fikriprayoga12234@gmail.com>
2021-05-24 04:37:20 +0700

    menambahkan  file controller

--- res.js
+++ res.js
@@ -1,8 +1,8 @@
 'use strict';
 
-exports.ok = function (value, res) {
+exports.ok = function (values, res) {
     var data = {
-        'status': 200
+        'status': 200,
         'values': values
     };
 
